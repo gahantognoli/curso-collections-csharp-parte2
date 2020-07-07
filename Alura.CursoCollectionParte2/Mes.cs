3e62e96384609dc401a21792d50ea3224d817d31
Gabriel Antognoli <40964619+gahantognoli@users.noreply.github.com>
2020-07-07 20:03:14 -0300

    Consultas com LINQ.

---
+++ Alura.CursoCollectionParte2/Mes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Alura.CursoCollectionParte2
+{
+    public class Mes //: IComparable
+    {
+        public Mes(string nome, int dias)
+        {
+            Nome = nome;
+            Dias = dias;
+        }
+
+        public string Nome { get; private set; }
+        public int Dias { get; private set; }
+
+        //public int CompareTo(object obj)
+        //{
+        //    if (obj == null)
+        //        return -1;
+
+        //    var mes = obj as Mes;
+
+        //    return this.Nome.CompareTo(mes.Nome);
+        //}
+
+        public override string ToString()
+        {
+            return $"[Nome: {Nome} - Dias: {Dias}]";
+        }
+    }
+}
--- Alura.CursoCollectionParte2/Program.cs
+++ Alura.CursoCollectionParte2/Program.cs
@@ -1,11 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Alura.CursoCollectionParte2
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            Aula3();
+
+            Console.ReadKey();
+        }
+
+        private static void Aula3()
+        {
+            List<Mes> meses = new List<Mes>()
+            {
+                new Mes("Janeiro", 31),
+                new Mes("Fevereiro", 28),
+                new Mes("Março", 31),
+                new Mes("Abril", 30),
+                new Mes("Maio", 31),
+                new Mes("Junho", 30),
+                new Mes("Julho", 31),
+                new Mes("Agosto", 31),
+                new Mes("Setembro", 30),
+                new Mes("Outubro", 31),
+                new Mes("Novembro", 30),
+                new Mes("Dezembro", 31)
+            };
+
+            //meses.Sort();
+            //foreach (var mes in meses)
+            //{
+            //    if (mes.Dias == 31)
+            //    {
+            //        Console.WriteLine(mes);
+            //    }
+            //}
+
+            var consulta = meses
+                .Where(m => m.Dias == 31)
+                .OrderBy(m => m.Nome)
+                .Select(m => m.Nome.ToUpper());
+
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Aula2()
         {
             //string[,] resultados = new string[3, 4]
             //{
@@ -49,8 +95,6 @@ namespace Alura.CursoCollectionParte2
             {
                 Console.WriteLine(string.Join(",", familia));
             }
-
-            Console.ReadKey();
         }
 
         private static void Aula1()
